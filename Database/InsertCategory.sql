c89c90c27649bf24b3e3c5d0554563ba9d4d26ea
Khoa Nguyen Thanh <>
2022-12-14 11:56:51 +0700

    update

--- Database/InsertCategory.sql
+++
@@ -1,3 +0,0 @@
-﻿INSERT INTO [dbo].[Category] ([CategoryName],[ParentId],[UrlName]) VALUES ('Home', 0, null)
-INSERT INTO [dbo].[Category] ([CategoryName],[ParentId],[UrlName]) VALUES ('Công nghệ - Game', 10, 'cong-nghe-game')
-INSERT INTO [dbo].[Category] ([CategoryName],[ParentId],[UrlName]) VALUES ('Sản phẩm', 11, 'san-pham')
