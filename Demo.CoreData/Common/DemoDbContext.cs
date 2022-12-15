6951eec1b2ba32c51419b1b4cc40299b7dd5bc22
Khoa Nguyen Thanh <chelsea.edenhazard16@gmail.com>
2022-12-15 15:11:16 +0700

    update

--- Demo.CoreData/Common/DemoDbContext.cs
+++ Demo.CoreData/Common/DemoDbContext.cs
@@ -26,7 +26,7 @@ public partial class DemoDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:crawlarticle.database.windows.net,1433;Initial Catalog=CoreData;Persist Security Info=False;User ID=khoant1405;Password=Khoa14051997;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer("Server=tcp:khoa-test.database.windows.net,1433;Initial Catalog=CoreData1;Persist Security Info=False;User ID=khoa-nt;Password=12345678@Abcd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
