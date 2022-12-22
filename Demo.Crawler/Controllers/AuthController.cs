fdf4eac728508219004843fa6cbd28c00545eeae
Khoa Nguyen Thanh <chelsea.edenhazard16@gmail.com>
2022-12-22 16:15:17 +0700

    refactor

--- Demo.Crawler/Controllers/AuthController.cs
+++ Demo.Crawler/Controllers/AuthController.cs
@@ -168,11 +168,9 @@ public class AuthController : ControllerBase
 
     private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
-        using (var hmac = new HMACSHA512())
-        {
-            passwordSalt = hmac.Key;
-            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
-        }
+        using var hmac = new HMACSHA512();
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
     }
 
     private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
