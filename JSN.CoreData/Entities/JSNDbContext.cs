7e92be47f1bd21a56da91d0b0a55c1e303cf8a36
Khoa Nguyen Thanh <chelsea.edenhazard16@gmail.com>
2022-12-26 09:35:13 +0700

    update

--- JSN.CoreData/Entities/JSNDbContext.cs
+++ JSN.CoreData/Entities/JSNDbContext.cs
@@ -25,7 +25,7 @@ public partial class JSNDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https: //go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer(
-            "Server=localhost,1433;User ID=sa;Password=1405;TrustServerCertificate=True;Initial Catalog=CoreData;");
+            "Server=tcp:newcoreserver.database.windows.net,1433;Initial Catalog=CoreData;Persist Security Info=False;User ID=khoant;Password=Abc12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
